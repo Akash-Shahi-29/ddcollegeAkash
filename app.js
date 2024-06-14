ba1b66bba4c8ce8e76eab960468dd1d1d84763fe
akash shahi <akashahi2004@gmail.com>
2024-06-14 13:45:45 +0530

    nothing

---
+++ app.js
@@ -0,0 +1,83 @@
+// let content = 2122323;
+// alert(content);
+
+// let typesOfVegetables = [
+//     "ladyfinger",
+//     "potato",
+//     "capcicum",
+//     "onion",
+//     "mushrooms"];
+
+// alert(typesOfVegetables);
+// alert(typesOfVegetables[3]);
+
+// let adharCard = {
+//     name: "Akash shahi",
+//     address: "selakui nigam rd",
+//     age: "20",
+//     no: 2323232332
+// };
+
+// alert(adharCard.address);
+
+// let age = 30*10;
+// alert(age);
+
+// let mohitAge=50;
+// let akashAge=mohitAge-30;
+// alert(akashAge);
+
+// let rajuAge=mohitAge+40;
+// alert(rajuAge);
+
+// let age=40;
+
+// let adultAge;   
+
+// function calculateAge(){
+//     return age+10;
+// }
+// adultAge=calculateAge();
+// console.log(adultAge);
+
+// age=20;
+// adultAge=calculateAge();
+// console.log(adultAge);
+
+
+// let hobby={
+//     abc(){
+//         alert("sports");
+//     }
+// };
+// alert(hobby.abc());
+
+
+// console.log(10+2);
+// console.log(10-2);
+// console.log(10*2);
+// console.log(10%4);
+
+// let result=40*9;
+// result++; //result++=result+1
+// console.log(result);
+
+// let result=40*9;
+// result--; //result--=result-1
+// console.log(result);
+
+
+// 
+// 
+
+// let add="akash"+ 29+40;
+// console.log(add);
+
+// let min=59-40;
+// console.log(min);
+
+// let bodm=((10*9)+4-50/20);
+// console.log(bodm);
+
+let myName="amit";
+console.log(myName.length);
---
+++ test.js
@@ -0,0 +1,43 @@
+// 1) Create three new variables:
+//    - A variable that stores the name of an DD College course of your choice
+//    - A variable that stores the price of that DD College course
+//    - A variable that stores the three “main goals” that you have, when taking this DD College course
+// 2) Output ("alert") the three variable values
+// let course="BSC CS";
+// let price=200000;
+// let goals=["I want to build carrer in the field of IT",
+//     "I want to learn with a proffesional teachers and in a good education environment in order to improve my skills",
+//     "I want to get placement in reputated IT companies with better annual package",
+// ];
+// alert(course);
+// alert(price);
+// alert(goals);
+// // 3) Try "grouping" the three variables together and still output their values thereafter
+// let ddCollege={
+// course: "BSC CS",
+// price: 200000,
+// goals: ["I want of IT",
+//     ,"I want to l education environment in order to improve my skills",
+//      "mpanies with better annual package"]};
+// alert(ddCollege.course);
+// alert(ddCollege.price);
+// alert(ddCollege.goals);
+// // 4) Also output the second element in your "main goals" variable
+// alert(goals[1]);
+// 5) Add a custom command (Function) that does the following:
+//    - Use your "main goals" variable and access an element by its identifier
+//    - The concrete identifier value should be dynamic / flexible 
+//      (i.e. the command can be executed for different identifier)
+//    - The "main goals" variable should also be dynamic: The command should work 
+//      with ANY list of values
+//    - The custom command should provide the accessed value (i.e. the list element)
+// let goals=["placement",
+//     "fees structure ",
+//     "placements"];
+
+// function ap(abc,def){
+//     xyz=abc[def];
+//     return xyz;
+// }
+// let ans=ap(goals,1);
+// alert(ans);
